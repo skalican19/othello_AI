14b5fe798d3277389fe91b71e6c77bfab214c945
skaliacan19 <michalskalican35@gmail.com>
2021-12-20 00:36:44 +0100

    can now find possible moves

--- apc_zad6_skalican/main.cpp
+++ apc_zad6_skalican/main.cpp
@@ -2,13 +2,15 @@
 #include <iostream>
 #include <string>
 #include <algorithm>
+#include <vector>
 
-
+constexpr size_t BOARD_SIZE = 8; 
 
 struct game {
-    bool color; //0 - black, 1 - white
+    bool active;
+    bool player_color; //0 - black, 1 - white
     float move_time;
-    float curr_time;
+    std::string last_state;    
 };
 
 struct command {
@@ -18,8 +20,68 @@ struct command {
     float time;
 };
 
+struct move {
+    size_t x;
+    size_t y;
+};
+
 bool is_number(std::string s) {
     return !s.empty() && std::all_of(s.begin(), s.end(), ::isdigit); 
+}
+
+bool check_direction(const std::string state, const bool color, const move move, const int dir_x, const int dir_y) {
+    size_t x = move.x + dir_x;
+    size_t y = move.y + dir_y;
+    size_t count = 0;
+
+    const auto c_op = color ? 'X' : 'O';
+    const auto c_self = !color ? 'X' : 'O';
+    
+    while (x < BOARD_SIZE && y < BOARD_SIZE && state[x * BOARD_SIZE + y] == c_op) {
+        count++;
+        x += dir_x;
+        y += dir_y;
+    }
+    
+    return x < BOARD_SIZE && y < BOARD_SIZE && count >= 1 && state[x * BOARD_SIZE + y] == c_self;
+}
+
+bool check_diagonals(const std::string state, const bool color, const move move) {
+    return check_direction(state, color, move, -1, -1) ||
+        check_direction(state, color, move, -1, 1) ||
+        check_direction(state, color, move, 1, -1) ||
+        check_direction(state, color, move, 1, 1);
+}
+
+bool check_straights(std::string state, bool color, move move) {
+    return check_direction(state, color, move, 0, -1) ||
+        check_direction(state, color, move, 0, 1) ||
+        check_direction(state, color, move, 1, 0) ||
+        check_direction(state, color, move, -1, 0);
+}
+
+std::vector<move> find_moves(const std::string state, const bool color) {
+    std::vector<move> moves;
+    
+    for (size_t x = 0; x < BOARD_SIZE; x++) {
+        for (size_t y = 0; y < BOARD_SIZE; y++) {
+            if (state[x*BOARD_SIZE+y] == '-')
+            if (check_diagonals(state, color, { x,y }) || check_straights(state, color, { x,y })) {
+                moves.push_back(move{ x,y });
+            }
+        }
+    }
+
+    return moves;
+}
+
+game init_game_struct(command cmd) {
+    game game{ true, cmd.player_color, cmd.time };
+
+    if (!cmd.player_color)
+        game.last_state = std::string("---------------------------OX------XO---------------------------");
+
+    return game;
 }
 
 bool move_valid(std::string move) {
@@ -82,17 +144,27 @@ command parse_command(const std::string input) {
     return cmd;
 }
 
-
 int main()
 {
     std::string command;
+    game game = { false };
     while (std::getline(std::cin, command))
     {
         std::string result;
         try {
             auto cmd = parse_command(command);
             if (cmd.type == "STOP") { return EXIT_SUCCESS; }
-            std::cout << cmd.type << " " << cmd.player_color << " " << cmd.time << std::endl;
+            if (cmd.type == "START" && !game.active) {
+                game = init_game_struct(cmd);
+                
+                if (!game.player_color) {
+                    //make a move
+                }
+                
+            }
+            if (game.active && cmd.type == "MOVE") {
+                std::vector<move> moves = find_moves(cmd.move, cmd.player_color);
+            }
         }
 
         catch (std::string msg) {
